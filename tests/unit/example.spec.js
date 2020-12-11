8786cde7f00633bf3994d5aff89a5483c8602e21
Aleksandr Sadykov <sumerokr@gmail.com>
2020-12-11 17:08:25 +0100

    Update tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,9 +2,21 @@ import { mount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    mount(HelloWorld);
+  it("renders nothing when user is fetching", () => {
+    const wrapper = mount(HelloWorld);
 
-    expect(1).toBe(1);
+    console.log(wrapper.html());
+
+    expect(wrapper.text()).not.toContain("admin");
   });
+
+  // try to remove this comment. Tests will be fixed!
+  // it("renders name when user is fetched", async () => {
+  //   const wrapper = mount(HelloWorld);
+
+  //   const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  //   await delay(500);
+
+  //   expect(wrapper.text()).toContain("admin");
+  // });
 });
