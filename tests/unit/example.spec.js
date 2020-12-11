7c9495abd641671890f15a77707affb6fcbb73bd
Aleksandr Sadykov <sumerokr@gmail.com>
2020-12-11 16:46:26 +0100

    Update tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,20 @@
 import { mount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    mount(HelloWorld);
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-    expect(1).toBe(1);
+describe("HelloWorld.vue", () => {
+  it("renders nothing when user is fetching", () => {
+    const wrapper = mount(HelloWorld);
+
+    expect(wrapper.text()).not.toContain("admin");
+  });
+
+  it("renders name when user is fetched", async () => {
+    const wrapper = mount(HelloWorld);
+
+    await delay(500);
+
+    expect(wrapper.text()).toContain("admin");
   });
 });
