597b76338b92f0d6457e9764ff208503ed797d9d
Aleksandr Sadykov <sumerokr@gmail.com>
2020-12-11 15:54:09 +0100

    Activate mswjs for production

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
